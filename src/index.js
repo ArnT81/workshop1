84f914bc78a8c785099dcf47bff46704ad770454
Åsa Jonsson <asaj71@gmail.com>
2021-11-22 20:45:07 +0100

    adding the students ad its logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DataTable from './table/DataTable';
+// import the js file, as we whant to be the 'startup' that will be renderd by the doom
 
-const Greeting = () => {
+// Creating a arrow function to return a greeting to test code will reder out.
+/*const Greeting = () => {
   return <h1>Hello Coders!</h1>;
-}
+}*/
 
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+//ReactDOM.render(<Greeting />, document.getElementById('root'));
+// Render the greeting function.
+ReactDOM.render(<DataTable/>, document.getElementById('root'));
 
 
 
---
+++ src/table/DataTable.js
@@ -0,0 +1,102 @@
+import React,{ useState } from "react";
+
+const DataTable = () => {
+
+  // the initial state with objects containing the student data/properties.
+    const initialData = [
+        {id: 1, firstName: "Dart", lastName: "Vader", age: 100, birthDate: "1921-01-01", country: "USA", city: "Los Angeles" },
+        {id: 2, firstName: "Åsa", lastName: "Jonsson", age: 50, birthDate: "1971-04-15", country: "Sweden", city: "Växjö" },
+        {id: 3, firstName: "Shalini", lastName: "Jonsson", age: 28, birthDate: "1993-03-29", country: "Sweden", city: "Växjö" },
+        {id: 4, firstName: "Arvid", lastName: "Larsson", age: 45, birthDate: "1976-10-07", country: "Sweden", city: "Ronneby" },
+        {id: 5, firstName: "Towa", lastName: "Jämsborn", age: 56, birthDate: "1965-04-17", country: "Sweden", city: "Kalmar" }
+    ];
+    
+    const [studentList, setStudentList] = useState(initialData);
+    // showDetails state for displaying student details with a boolean,the button onclick function displayData returns a true value and the properties
+    const [showDetails, setShowDetails] = useState(false);
+    const studentDefaultData = {id: 0, firstName: "", lastName: "", age: 0, birthDate: "", country: "", city: "" }
+    const [student, setStudent] = useState(studentDefaultData);
+// function that returns table header
+const TableHeader = () => {
+    return (
+      
+    <thead className="table-light">
+        <td>Id</td>
+        <td>FirstName</td>
+        <td>LastName</td>
+        <td>Age</td>
+        <td>Action</td>
+    </thead>
+    );
+};
+
+const TableRow = (props) => {
+    return(
+    <tbody>
+        {
+            props.list.map((student) => (
+            <tr key={student.id}>
+                <td>{student.id}</td>
+                <td>{student.firstName}</td>
+                <td>{student.lastName}</td>
+                <td>{student.age}</td>
+                <td><TableAction student={student} /></td>
+            </tr>
+                ) )
+        }
+    </tbody>
+    );
+};
+
+const TableAction = (props) => {
+
+    const displayData = () => {
+        setShowDetails(true);
+        console.log("Show something",props.student);
+        setStudent(props.student);
+    };
+
+    return (<button type="button" className="btn btn-primary" onClick={displayData} >Details</button>);
+
+};
+
+const ShowStudentDetails = () => {
+
+    if(showDetails){
+        return(
+        <div className="card" style={{width:'400px'}}>
+          <div className="card-header bg-info text-white">
+              Student Information
+          </div>
+          <div className="card-body">
+              <h5 className="card-title"> {student.country }: {student.city }</h5>
+              <p className="card-text">ID: {student.id}</p>
+              <p className="card-text">Name: {student.firstName} {student.lastName}</p>
+              <p className="card-text">BirthDate: {student.birthDate}</p>
+          </div>
+          <div className="card-footer">
+              <button type="button" className="btn btn-info" onClick={()=> {setShowDetails(false); setStudent(studentDefaultData)}}>Hide info</button>
+          </div>
+      </div>
+        );
+    } else {
+        return ("");
+    }
+    
+
+};
+
+    return (
+        <div className="container"> 
+            <h3>Student List</h3>
+            <table className="table table-striped">
+                <TableHeader/> 
+                <TableRow list={studentList} />
+            </table>
+            <br/>
+            <ShowStudentDetails />
+        </div>
+    );
+};
+
+export default DataTable;
